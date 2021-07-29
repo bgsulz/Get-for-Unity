a32092c03bfec0517465c1c10d8c797ceb897796
bgsulz <38191432+bgsulz@users.noreply.github.com>
2021-07-28 20:41:06 -0400

    Update GetPropertyDrawer.cs

--- Editor/GetPropertyDrawer.cs
+++ Editor/GetPropertyDrawer.cs
@@ -1,53 +1,141 @@
-﻿using BGS.Attributes;
+// ReSharper disable file CoVariantArrayConversion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-[CustomPropertyDrawer(typeof(GetAttribute), true)]
-public class GetPropertyDrawer : PropertyDrawer
+namespace Extra.Attributes
 {
-    private GetAttribute GetAttribute => (GetAttribute)attribute;
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(GetAttribute), true)]
+    public class GetPropertyDrawer : PropertyDrawer
     {
-        GetComponent(property, GetAttribute.GetterSource);
-        
-        EditorGUI.BeginProperty(position, label, property);
-        GUI.enabled = false;
-        GUI.backgroundColor = property.objectReferenceValue == null ? Color.red : Color.white;
-        EditorGUI.PropertyField(position, property, new GUIContent(label.text + " " + LabelText(GetAttribute.GetterSource)));
-        EditorGUI.EndProperty();
+        #region Helpers
+
+        private GetAttribute GetAttribute => (GetAttribute) attribute;
+
+        #endregion
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var listObjects = property.CandidateObjects(GetAttribute.GetterSource, fieldInfo);
+
+            if (listObjects.Length <= 0)
+            {
+                GUI.enabled = false;
+                EditorGUI.LabelField(position, label.GetterFormat(GetAttribute));
+                
+                var messagePosition = position.MessageRect();
+                
+                GUI.enabled = true;
+                if (GetAttribute.GetterSource == GetterSource.Find) 
+                    GUI.Label(messagePosition, "None found.");
+                else if (GUI.Button(messagePosition, "None found. Add?")) 
+                    property.SetRef(property.Target()?.gameObject.AddComponent(fieldInfo.FieldType));
+                
+                return;
+            }
+
+            var index = Mathf.Clamp(Array.IndexOf(listObjects, property.Ref()), 0, int.MaxValue);
+
+            EditorGUI.BeginChangeCheck();
+
+            var newIndex = EditorGUI.Popup(position.RightSide(), index, listObjects.ToFormattedNames().ToArray());
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                var oldRef = property.Ref();
+                property.SetRef(listObjects[newIndex]);
+                
+                if (oldRef != property.Ref())
+                {
+                    Undo.RecordObject(property.serializedObject.targetObject, "Changed property value");
+                    property.serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            GUI.enabled = false;
+            EditorGUI.ObjectField(position.LeftSide(), property, new GUIContent(label.GetterFormat(GetAttribute)));
+        }
     }
 
-    private string LabelText(GetterSource getterSource) => getterSource switch
-        {
-            GetterSource.Object => "[Auto Get]", 
-            GetterSource.Children => "[Auto Get Children]", 
-            GetterSource.Parent => "[Auto Get Parent]", 
-            GetterSource.Find => "[Auto Find]", 
-            GetterSource.Add => "[Auto Get/Add]", 
-            _ => "[Auto Get]"
-        };
-
-    private void GetComponent(SerializedProperty property, GetterSource getterSource)
+    internal static class GetPropertyDrawerExtensions
     {
-        var mb = property.serializedObject.targetObject as MonoBehaviour;
+        internal static MonoBehaviour Target(this SerializedProperty property) 
+            => property.serializedObject.targetObject as MonoBehaviour;
         
-        var component = mb == null ? null : getterSource switch
-        {
-            GetterSource.Object => mb.GetComponent(fieldInfo.FieldType), 
-            GetterSource.Children => mb.GetComponentInChildren(fieldInfo.FieldType), 
-            GetterSource.Parent => mb.GetComponentInParent(fieldInfo.FieldType), 
-            GetterSource.Find => Object.FindObjectOfType(fieldInfo.FieldType), 
-            GetterSource.Add => GetOrAdd(mb, fieldInfo.FieldType), 
-            _ => null
-        };
-        
-        property.objectReferenceValue = component ? component : null;
-    }
+        internal static Object Ref(this SerializedProperty property)
+            => property.objectReferenceValue;
 
-    private Component GetOrAdd(MonoBehaviour enactor, System.Type type)
-    {
-        var tryGet = enactor.GetComponent(type);
-        return tryGet ? tryGet : enactor.gameObject.AddComponent(type);
+        internal static Object SetRef(this SerializedProperty property, in Object value) 
+            => property.objectReferenceValue = value;
+
+        internal static string GetterFormat(this GUIContent label, in GetAttribute attribute)
+            => $"{label.text} [src: {attribute.GetterSource.ToString()}]";
+        
+        internal static Rect LeftSide(this Rect position)
+        {
+            var res = position;
+            res.width *= 0.8f;
+            res.x = position.x;
+            return res;
+        }
+
+        internal static Rect RightSide(this Rect position)
+        {
+            var res = position;
+            res.width *= 0.2f;
+            res.x = position.x + position.width * 0.8f;
+            return res;
+        }
+        
+        internal static Rect MessageRect(this Rect position)
+        {
+            var res = position;
+            res.width = position.width * 0.6f;
+            res.x = position.x + position.width * 0.4f;
+            return res;
+        }
+        
+        internal static IEnumerable<string> ToFormattedNames(this IEnumerable<Object> input)
+        {
+            var names = input.Select(item => item.name).ToArray();
+
+            var indexTrackerDictionary = names
+                .GroupBy(item => item)
+                .Where(item => item.Count() > 1)
+                .ToDictionary(item => item.Key, item => 0);
+
+            foreach (var item in names)
+            {
+                if (!indexTrackerDictionary.TryGetValue(item, out var value)) // If not a duplicate...
+                {
+                    yield return item;
+                    continue;
+                }
+                
+                indexTrackerDictionary[item] = ++value; // We've now encountered this item {value} times.
+                yield return $"{item} [{value - 1}]";
+            }
+        }
+
+        internal static Object[] CandidateObjects(this SerializedProperty property, in GetterSource getterSource, in FieldInfo fieldInfo)
+        {
+            var mb = Target(property);
+            
+            return mb
+                ? getterSource switch
+                {
+                    GetterSource.Object => mb.GetComponents(fieldInfo.FieldType),
+                    GetterSource.Children => mb.GetComponentsInChildren(fieldInfo.FieldType, true),
+                    GetterSource.Parent => mb.GetComponentsInParent(fieldInfo.FieldType, true),
+                    GetterSource.Find => Object.FindObjectsOfType(fieldInfo.FieldType, true),
+                    _ => null
+                }
+                : null;
+        }
     }
 }
